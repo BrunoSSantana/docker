0744a3a3aa5b1173eae93beeb04b69ef6f89c9cd
BrunoSSantana <brunoosouza15@gmail.com>
2021-12-09 21:10:14 -0300

    add app

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 
 const app = express()
 
 app.get('/', (request, response) => {
-  response.json({message: "Hello"})
+  response.json({message: `Bruno`})
 })
 
-app.listen(3003, console.log('🚀'))
+app.listen(3003, console.log('🚀 http://localhost:3003'))
