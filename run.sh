40bb491512b3fd14ecf1c9674de8cf9a64223c21
BrunoSSantana <brunoosouza15@gmail.com>
2021-12-10 23:47:06 -0300

    update shell

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 if test $1 = 'prod'
-then
-  heroku container:push web
-  heroku container:release web
+  then
+    heroku container:push web && heroku container:release web && heroku open
 fi
+
 if test $1 = 'dev'
-then
-  docker-compose up --build
+  then
+    docker-compose up --build
   else
-  echo 'comando errado amigo!'
+    echo 'comando errado amigo!\nOpções: prod ou dev'
 fi
